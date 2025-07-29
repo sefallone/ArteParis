62e4a24939ca37b64b053ce87bcdf2854f4f469d
sefallone <166940959+sefallone@users.noreply.github.com>
2025-07-30 01:05:09 +0200

    db.py

--- db.py
+++ db.py
@@ -4,14 +4,14 @@ import streamlit as st
 def get_connection():
     try:
         conn = mysql.connector.connect(
-            host=st.secrets["mysql"]["host"],
-            user=st.secrets["mysql"]["user"],
-            password=st.secrets["mysql"]["password"],
-            database=st.secrets["mysql"]["database"],
-            autocommit=True,
-            ssl_ca="/etc/ssl/certs/ca-certificates.crt",
-            ssl_verify_cert=True
-        )
+        host=st.secrets["mysql"]["host"],
+        user=st.secrets["mysql"]["user"],
+        password=st.secrets["mysql"]["password"],
+        database=st.secrets["mysql"]["database"],
+        autocommit=True,
+        ssl_verify_cert=True
+    )
+
         return conn
     except mysql.connector.Error as err:
         st.error(f"Error al conectar a la base de datos: {err}")
