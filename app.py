f99c401bf61e0504c2a9596ac5935fc669a67383
sefallone <166940959+sefallone@users.noreply.github.com>
2025-07-30 12:02:03 +0200

    app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import streamlit as st
 from db import get_connection
 
 def main():
-    st.title("Conexión de prueba a PlanetScale")
+    st.title("Prueba de conexión a PlanetScale")
 
     try:
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("SHOW TABLES")
         tablas = cursor.fetchall()
-        st.success("✅ Conexión exitosa")
-        st.write("Tablas:", tablas)
-    except:
+        st.success("✅ Conexión exitosa a la base de datos")
+        st.write("Tablas en la base de datos:", tablas)
+    except Exception:
         st.error("❌ Conexión fallida")
 
 if __name__ == "__main__":
