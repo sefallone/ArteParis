81e4dfa54491d5778c4c938e8d5d88b7bed20fc5
sefallone <166940959+sefallone@users.noreply.github.com>
2025-07-30 15:31:47 +0200

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
 from db import crear_tabla_productos, obtener_productos
+import pandas as pd
 
 def main():
     st.title("Inventario Arte París")
-
-    # Crear tabla productos si no existe (solo la primera vez)
+    
     crear_tabla_productos()
 
-    # Mostrar productos
     productos = obtener_productos()
 
     if productos:
-        # Mostrar en dataframe
-        st.dataframe(productos)
+        df = pd.DataFrame(productos, columns=["ID", "Nombre", "Cantidad", "Precio Costo", "Precio Venta"])
+        st.dataframe(df)
     else:
-        st.write("No hay productos en el inventario.")
+        st.info("No hay productos en la base de datos.")
 
 if __name__ == "__main__":
     main()
