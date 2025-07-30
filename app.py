6763fca0a473a37bf5a36912b716c992690b844c
sefallone <166940959+sefallone@users.noreply.github.com>
2025-07-30 15:23:39 +0200

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
-# app.py
 import streamlit as st
-from db import crear_tabla_productos, obtener_productos
 
 def main():
     st.title("Inventario Arte París")
-
-    crear_tabla_productos()  # Aseguramos que la tabla exista
-
-    productos = obtener_productos()
-
-    if productos:
-        st.table(productos)
-    else:
-        st.write("No hay productos aún.")
+    st.write("Esto es un test para ver si se muestra algo")
 
 if __name__ == "__main__":
     main()
