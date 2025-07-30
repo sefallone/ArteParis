6bb54ec204310a76322e91f82342fd9dad903d83
sefallone <166940959+sefallone@users.noreply.github.com>
2025-07-30 15:58:10 +0200

    db.py

--- db.py
+++ db.py
@@ -1,29 +1,23 @@
-import psycopg2
-import streamlit as st
+import sqlite3
+
+DB_PATH = "inventario.db"
 
 def get_connection():
-    return psycopg2.connect(
-        host=st.secrets["postgres"]["host"],
-        user=st.secrets["postgres"]["user"],
-        password=st.secrets["postgres"]["password"],
-        database=st.secrets["postgres"]["database"],
-        port=st.secrets["postgres"]["port"]
-    )
+    return sqlite3.connect(DB_PATH)
 
 def crear_tabla_productos():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS productos (
-        id SERIAL PRIMARY KEY,
-        nombre VARCHAR(100) NOT NULL,
-        cantidad INT NOT NULL,
-        precio_costo NUMERIC(10,2),
-        precio_venta NUMERIC(10,2)
-    );
+        CREATE TABLE IF NOT EXISTS productos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            cantidad INTEGER NOT NULL,
+            precio_costo REAL,
+            precio_venta REAL
+        );
     """)
     conn.commit()
-    cursor.close()
     conn.close()
 
 def obtener_productos():
@@ -31,6 +25,15 @@ def obtener_productos():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM productos;")
     resultados = cursor.fetchall()
-    cursor.close()
     conn.close()
     return resultados
+
+def agregar_producto(nombre, cantidad, precio_costo, precio_venta):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO productos (nombre, cantidad, precio_costo, precio_venta)
+        VALUES (?, ?, ?, ?)
+    """, (nombre, cantidad, precio_costo, precio_venta))
+    conn.commit()
+    conn.close()
